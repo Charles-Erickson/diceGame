977d70ba49f80561f63695ccab5402f8aca762d3
unknown <cse621@gmail.com>
2019-03-19 08:48:13 -0500

    game rule fucntion and card arrays added

--- diceGameJavascript.js
+++ diceGameJavascript.js
@@ -13,13 +13,13 @@ if(amount==1){
 	console.log("Paper");
 }
 else if(amount==2){
-console.log('Rock');
+	console.log('Rock');
 }
 else if(amount==3){
 	console.log('Scissors')
 }
 }
-rockPaperScissors();
+
 function fourSidedDice(){
 	let min=1
 	let max=5
@@ -82,48 +82,74 @@ function rollDice(number){
 		fourteenSidedDice();
 	}		
 	else{
-		console.log('Enter a real dice amount');
+		prompt('Enter a real dice amount');
+	}
+}
+function gameRules(playerOneInput,playerTwoInput){
+	if(playerOneInput=="Paper" && playerTwoInput=="Rock"){
+		alert('Player One Wins!!!!')
+	}
+	else if(playerOneInput=='Scissors' && playerTwoInput=='Paper'){
+		alert('Player One Wins!!!!')
+	}
+	else if(playerOneInput=='Rock' && playerTwoInput=='Scissors'){
+		alert('Player One Wins!!!!')
+	}
+	else if(playerTwoInput=='Paper' && playerOneInput=="Rock"){
+		alert('Player Two Wins!!!!')
+	}
+	else if(playerTwoInput=='Rock' && playerOneInput=='Scissors'){
+		alert('Player Two Wins!!!!')
+	}
+	else if(playerTwoInput=='Scissors' && playerOneInput=="Paper"){
+		alert('Player Two Wins!!!!')
+	}
+	else if( playerOneInput===playerTwoInput){
+		alert('Draw')
+	}
+	else(){
+		prompt('redo')
 	}
 }
 
 function getRPSCards(){
-	let input= prompt('Enter Dice size');
+	let input= prompt('Enter Dice size 4, 6, 8, 10, 12, 14');
 	let rSPRoll=rollDice(input);
 	let i=0;
 	do{
-		rockPaperScissors(); i++;
+		let result=rockPaperScissors(); i++;
+		console.log(result)
 	}
-	while(i<rSPRoll){
-		
-	}
+	while(i<rSPRoll)
 }
 
 function paperArray(){
 	let newArray=[];
-	let input=prompt('Enter dice size');
+	let input=prompt('PAPER: Enter Dice size 4, 6, 8, 10, 12, 14');
 	do{
-		let paperNumber= rollDice(input);
-	while(rockPaperScissors()=="Paper")}
+		let paperNumber= rollDice(input)
+	}
+	while(rockPaperScissors()=="Paper");
 	let paperArray=newArray.push(paperNumber);
 }
 
 function rockArray(){
 	let newArray=[];
-	let input= prompt('Enter dice size');
+	let input= prompt('ROCK: Enter Dice size 4, 6, 8, 10, 12, 14');
 	do{
-		let rockNumber= rollDice(input);
-		while(rockPaperScissors()=="Rock")
+		let rockNumber= rollDice(input)
 	}
+		while(rockPaperScissors()=="Rock");
 	let rockArray=newArray.splice(0,rockNumber);
 }
 
 function scissorArray(){
 	let newArray=[];
-	let input=prompt('Enter dice size');
+	let input=prompt('SCISSORS: Enter Dice size 4, 6, 8, 10, 12, 14');
 	do{
-		let scissorNumber= rollDice(input);
-		while(rockPaperScissors()=="Scissors")
+		let scissorNumber= rollDice(input)
 	}
+		while(rockPaperScissors()=="Scissors");
 	let scissorArray=newArray.splice(0,1,scissorNumber)
 }
 
@@ -131,4 +157,28 @@ function cardArray(){
 	let cardArray=paperArray()+scissorArray()+rockArray();
 }
 
-function 
+// function subtractArray(){
+
+// }
+
+function playerOneInsert(){
+	let input= prompt('Player One enter Paper, Rock, or Scissors');
+	let newArray;
+	if(input= 'Paper'){
+		newArray=cardArray()-[1,0,0]
+	}
+	else if(input='Rock'){
+		newArray=cardArray()-[0,1,0]
+	}
+	else if(input='Scissors'){
+		newArray=cardArray()-[0,0,1]
+	}
+	else{
+		newArray=prompt('Please enter Paper, Rock, Scissors')
+	}
+
+	let 
+
+}
+
+getRPSCards()
